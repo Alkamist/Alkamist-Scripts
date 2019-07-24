036e5a109615a8259cf7da99f623b8402dc2ee6b
Corey Lehman <coreylehman@me.com>
2019-07-24 11:12:54 -0400

    Hiding tracks isn't working well.

--- Zoom Tool/Zoom Tool.lua
+++ Zoom Tool/Zoom Tool.lua
@@ -26,8 +26,6 @@ local initalTrackHeights = {}
 local focusedWindow =  reaper.JS_Window_GetFocus()
 local yAdjustment = 0
 
-local originallyVisibleTracks = {}
-
 
 
 function msg(m)
@@ -81,51 +79,6 @@ function moveVerticalScroll(value)
     reaper.JS_Window_SetScrollPos(focusedWindow, "VERT", math.max(math.floor(scrollPos + value), 0))
 end
 
-local tracksOnScreen = getTCPTracksOnScreen()
-local tempHiddenTracks = {}
-local hideScrollCorrection = 0
-local paddingTracksBeforeHide = 50
-function hideOffScreenTracks()
-    if trackIsValid(tracksOnScreen[1]) and trackIsValid(tracksOnScreen[#tracksOnScreen]) then
-        local firstOnScreenTrack = tracksOnScreen[1]
-        local firstOnScreenTrackNumber = reaper.GetMediaTrackInfo_Value(firstOnScreenTrack, "IP_TRACKNUMBER")
-        local lastOnScreenTrack = tracksOnScreen[#tracksOnScreen]
-        local lastOnScreenTrackNumber = reaper.GetMediaTrackInfo_Value(lastOnScreenTrack, "IP_TRACKNUMBER")
-
-        local hiddenTrackIndex = 1
-        for i = 1, #originallyVisibleTracks do
-            if trackIsValid(originallyVisibleTracks[i]) then
-                local currentTrack = originallyVisibleTracks[i]
-                local currentTrackNumber = reaper.GetMediaTrackInfo_Value(currentTrack, "IP_TRACKNUMBER")
-
-                if currentTrackNumber < firstOnScreenTrackNumber - paddingTracksBeforeHide - 1 then
-                    hideScrollCorrection = hideScrollCorrection + reaper.GetMediaTrackInfo_Value(currentTrack, "I_WNDH")
-                end
-
-                if currentTrackNumber < firstOnScreenTrackNumber - paddingTracksBeforeHide - 1 or currentTrackNumber > lastOnScreenTrackNumber + paddingTracksBeforeHide then
-                    reaper.SetMediaTrackInfo_Value(currentTrack, "B_SHOWINTCP", 0);
-                    reaper.SetMediaTrackInfo_Value(currentTrack, "B_SHOWINMIXER", 0);
-                    tempHiddenTracks[hiddenTrackIndex] = currentTrack
-                    hiddenTrackIndex = hiddenTrackIndex + 1
-                end
-            end
-        end
-
-        moveVerticalScroll(-hideScrollCorrection)
-    end
-end
-
-function showTempHiddenTracks()
-    for i = 1, #tempHiddenTracks do
-        if trackIsValid(tempHiddenTracks[i]) then
-            reaper.SetMediaTrackInfo_Value(tempHiddenTracks[i], "B_SHOWINTCP", 1);
-            reaper.SetMediaTrackInfo_Value(tempHiddenTracks[i], "B_SHOWINMIXER", 1);
-        end
-    end
-
-    moveVerticalScroll(hideScrollCorrection)
-end
-
 function zoomAllTracksToAdjustment()
     --reaper.PreventUIRefresh(1)
 
@@ -164,8 +117,6 @@ end
 
 function atExit()
     --zoomAllTracksToAdjustment()
-    --showTempHiddenTracks()
-    --reaper.TrackList_AdjustWindows(false)
 
     -- Stop intercepting keyboard input.
     reaper.JS_VKeys_Intercept(-1, -1)
@@ -222,21 +173,15 @@ function init()
     initialMousePos.x, initialMousePos.y = reaper.GetMousePosition()
     previousMousePos.x, previousMousePos.y = reaper.GetMousePosition()
 
-    local trackIndex = 1
     for i = 1, reaper.CountTracks(0) do
         local currentTrack = reaper.GetTrack(0, i - 1)
 
         if reaper.IsTrackVisible(currentTrack, false) then
             if trackIsValid(currentTrack) then
-                originallyVisibleTracks[trackIndex] = currentTrack
-
-                initalTrackHeights[trackIndex] = reaper.GetMediaTrackInfo_Value(currentTrack, "I_WNDH")
-                trackIndex = trackIndex + 1
+                initalTrackHeights[i] = reaper.GetMediaTrackInfo_Value(currentTrack, "I_WNDH")
             end
         end
     end
-
-    --hideOffScreenTracks()
 
     reaper.defer(update)
 end
@@ -257,30 +202,19 @@ function update()
     -- Horizontal zoom is easy.
     reaper.adjustZoom(xAdjustment, 0, true, -1)
 
+    local tracksOnScreen = getTCPTracksOnScreen()
+
     -- Vertical zoom is a bit more complicated.
     for i = 1, #tracksOnScreen do
         local currentTrack = tracksOnScreen[i]
         local currentTrackNumber = reaper.GetMediaTrackInfo_Value(currentTrack, "IP_TRACKNUMBER")
 
-        --local trackHeight = initalTrackHeights[currentTrackNumber] + yAdjustment
-        --local trackHeight = math.max(trackHeight, 0)
+        local trackHeight = initalTrackHeights[currentTrackNumber] + yAdjustment
+        local trackHeight = math.max(trackHeight, 0)
 
-        --reaper.SetMediaTrackInfo_Value(currentTrack, "I_HEIGHTOVERRIDE", trackHeight);
+        reaper.SetMediaTrackInfo_Value(currentTrack, "I_HEIGHTOVERRIDE", trackHeight);
     end
     reaper.TrackList_AdjustWindows(false)
-
-    --local _, scrollPos, scrollPageSize, scrollMin, scrollMax, scrollTrackPos = reaper.JS_Window_GetScrollInfo(focusedWindow, "VERT")
-    --local scrollScaleFactor = 7.0
-    --local newScrollPos = scrollPos + yRelativeAdjustment * scrollScaleFactor
-    --reaper.JS_Window_SetScrollPos(focusedWindow, "VERT", math.max(math.floor(testScroll), 0))
-
-    -- We need to scroll as we zoom to keep the track under the cursor in view.
-    --_, scrollPos, scrollPageSize, scrollMin, scrollMax, scrollTrackPos = reaper.JS_Window_GetScrollInfo(focusedWindow, "VERT")
-
-    --local scrollScaleFactor = 7.0
-    --local newScrollPos = math.max(scrollPos + yRelativeAdjustment * scrollScaleFactor, 0)
-
-    --reaper.JS_Window_SetScrollPos(focusedWindow, "VERT", math.floor(newScrollPos))
 
     reaper.PreventUIRefresh(-1)
 
