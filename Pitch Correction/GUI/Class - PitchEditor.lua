10b3b5c5a581d0a041ab110ae8fffc9e2664bdca
Corey Lehman <coreylehman@me.com>
2019-08-12 13:41:56 -0400

    Improved selection code more.

--- Pitch Correction/GUI/Class - PitchEditor.lua
+++ Pitch Correction/GUI/Class - PitchEditor.lua
@@ -242,44 +242,42 @@ function GUI.PitchEditor:handleCorrectionEditing()
         end
     end
 
-    for key, correction in PitchCorrection.pairs(self.pitchCorrections) do
-        if correction.isSelected == true then
-            -- Clear the pitch envelope under the correction before moving it.
-            if Lua.getTableLength(self.pitchPoints) > 0 then
-                local pitchEnvelope = self.pitchPoints[1]:getEnvelope()
-                local playrate = self.pitchPoints[1]:getPlayrate()
+    for key, correction in PitchCorrection.pairs(self.selectedPitchCorrections) do
+        -- Clear the pitch envelope under the correction before moving it.
+        if #self.pitchPoints > 0 then
+            local pitchEnvelope = self.pitchPoints[1]:getEnvelope()
+            local playrate = self.pitchPoints[1]:getPlayrate()
 
-                reaper.DeleteEnvelopePointRange(pitchEnvelope, playrate * correction.leftTime, playrate * correction.rightTime)
-            end
+            reaper.DeleteEnvelopePointRange(pitchEnvelope, playrate * correction.leftTime, playrate * correction.rightTime)
+        end
 
-            local mouseTimeChange = mouseTime - self.previousMouseTime
-            local maxRightTimeChange = mouseTime - correction.leftTime
-            local maxLeftTimeChange = mouseTime - correction.rightTime
+        local mouseTimeChange = mouseTime - self.previousMouseTime
+        local maxRightTimeChange = mouseTime - correction.leftTime
+        local maxLeftTimeChange = mouseTime - correction.rightTime
 
-            local mousePitchChange = snappedMousePitch - self.previousSnappedMousePitch
+        local mousePitchChange = snappedMousePitch - self.previousSnappedMousePitch
 
-            if self.justCreatedNewPitchCorrection then
+        if self.justCreatedNewPitchCorrection then
+            local change = math.min(mouseTimeChange, maxRightTimeChange)
+            correction.rightTime = math.max(correction.rightTime + change, correction.leftTime + self.minimumCorrectionTime)
+            correction.rightPitch = correction.rightPitch + mousePitchChange
+        else
+            if self.editHandle == "left" then
+                local change = math.max(mouseTimeChange, maxLeftTimeChange)
+                correction.leftTime = math.min(correction.leftTime + change, correction.rightTime - self.minimumCorrectionTime)
+                correction.leftPitch = correction.leftPitch + mousePitchChange
+
+            elseif self.editHandle == "right" then
                 local change = math.min(mouseTimeChange, maxRightTimeChange)
                 correction.rightTime = math.max(correction.rightTime + change, correction.leftTime + self.minimumCorrectionTime)
                 correction.rightPitch = correction.rightPitch + mousePitchChange
-            else
-                if self.editHandle == "left" then
-                    local change = math.max(mouseTimeChange, maxLeftTimeChange)
-                    correction.leftTime = math.min(correction.leftTime + change, correction.rightTime - self.minimumCorrectionTime)
-                    correction.leftPitch = correction.leftPitch + mousePitchChange
 
-                elseif self.editHandle == "right" then
-                    local change = math.min(mouseTimeChange, maxRightTimeChange)
-                    correction.rightTime = math.max(correction.rightTime + change, correction.leftTime + self.minimumCorrectionTime)
-                    correction.rightPitch = correction.rightPitch + mousePitchChange
+            elseif self.editHandle == "middle" then
+                correction.leftTime = correction.leftTime + mouseTimeChange
+                correction.leftPitch = correction.leftPitch + mousePitchChange
 
-                elseif self.editHandle == "middle" then
-                    correction.leftTime = correction.leftTime + mouseTimeChange
-                    correction.leftPitch = correction.leftPitch + mousePitchChange
-
-                    correction.rightTime = correction.rightTime + mouseTimeChange
-                    correction.rightPitch = correction.rightPitch + mousePitchChange
-                end
+                correction.rightTime = correction.rightTime + mouseTimeChange
+                correction.rightPitch = correction.rightPitch + mousePitchChange
             end
         end
     end
@@ -299,78 +297,9 @@ end
 function GUI.PitchEditor:ondrag()
     if self.item == nil then return end
 
-    local mouseTime = self:getTimeFromPixels(GUI.mouse.x)
-    local mousePitch = self:getPitchFromPixels(GUI.mouse.y)
-    local snappedMousePitch = self:getSnappedPitch(mousePitch)
-
-    -- The drag just started.
-    if not self.lWasDragged then
-        local mouseOriginalTime = self:getTimeFromPixels(GUI.mouse.ox, self.zoomXPreDrag, self.scrollXPreDrag)
-        local mouseOriginalPitch = self:getPitchFromPixels(GUI.mouse.oy, self.zoomYPreDrag, self.scrollYPreDrag)
-        local mouseOriginalSnappedPitch = self:getSnappedPitch(mouseOriginalPitch)
-
-        self.previousMouseTime = mouseOriginalTime
-        self.previousMousePitch = mouseOriginalPitch
-        self.previousSnappedMousePitch = self:getSnappedPitch(mouseOriginalPitch)
-
-        if self.editCorrection == nil then
-            self:createAndEditNewPitchCorrection(mouseOriginalTime, mouseOriginalTime, mouseOriginalSnappedPitch, mouseOriginalSnappedPitch)
-        end
-    end
-
-    for key, correction in PitchCorrection.pairs(self.pitchCorrections) do
-        if correction.isSelected == true then
-            -- Clear the pitch envelope under the correction before moving it.
-            if Lua.getTableLength(self.pitchPoints) > 0 then
-                local pitchEnvelope = self.pitchPoints[1]:getEnvelope()
-                local playrate = self.pitchPoints[1]:getPlayrate()
-
-                reaper.DeleteEnvelopePointRange(pitchEnvelope, playrate * correction.leftTime, playrate * correction.rightTime)
-            end
-
-            local mouseTimeChange = mouseTime - self.previousMouseTime
-            local maxRightTimeChange = mouseTime - correction.leftTime
-            local maxLeftTimeChange = mouseTime - correction.rightTime
-
-            local mousePitchChange = snappedMousePitch - self.previousSnappedMousePitch
-
-            if self.justCreatedNewPitchCorrection then
-                local change = math.min(mouseTimeChange, maxRightTimeChange)
-                correction.rightTime = math.max(correction.rightTime + change, correction.leftTime + self.minimumCorrectionTime)
-                correction.rightPitch = correction.rightPitch + mousePitchChange
-            else
-                if self.editHandle == "left" then
-                    local change = math.max(mouseTimeChange, maxLeftTimeChange)
-                    correction.leftTime = math.min(correction.leftTime + change, correction.rightTime - self.minimumCorrectionTime)
-                    correction.leftPitch = correction.leftPitch + mousePitchChange
-
-                elseif self.editHandle == "right" then
-                    local change = math.min(mouseTimeChange, maxRightTimeChange)
-                    correction.rightTime = math.max(correction.rightTime + change, correction.leftTime + self.minimumCorrectionTime)
-                    correction.rightPitch = correction.rightPitch + mousePitchChange
-
-                elseif self.editHandle == "middle" then
-                    correction.leftTime = correction.leftTime + mouseTimeChange
-                    correction.leftPitch = correction.leftPitch + mousePitchChange
-
-                    correction.rightTime = correction.rightTime + mouseTimeChange
-                    correction.rightPitch = correction.rightPitch + mousePitchChange
-                end
-            end
-        end
-    end
+    self:handleCorrectionEditing()
 
     self.lWasDragged = true
-    self.previousMouseTime = mouseTime
-    self.previousMousePitch = mousePitch
-    self.previousSnappedMousePitch = snappedMousePitch
-
-    if self.editCorrection then
-        self:applyPitchCorrections()
-        self:drawPreviewPitchLines()
-    end
-
-    self:drawPitchCorrections()
 
     self:redraw()
 end
@@ -1006,8 +935,10 @@ function GUI.PitchEditor:createAndEditNewPitchCorrection(leftTime, rightTime, le
 end
 
 function GUI.PitchEditor:selectPitchCorrection(correction)
-    correction.isSelected = true
-    table.insert(self.selectedPitchCorrections, correction)
+    if not correction.isSelected then
+        correction.isSelected = true
+        table.insert(self.selectedPitchCorrections, correction)
+    end
 end
 
 function GUI.PitchEditor:unselectAllPitchCorrections()
