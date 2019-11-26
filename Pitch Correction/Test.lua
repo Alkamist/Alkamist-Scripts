f4d00fde603c693ec83d2ddf56d298a7aedeef80
Corey Lehman <coreylehman@me.com>
2019-11-26 16:25:11 -0500

    Doing some weird experiments with functional state.

--- Pitch Correction/Test.lua
+++ Pitch Correction/Test.lua
@@ -17,12 +17,12 @@ GUI.initialize{
 }
 GUI.window.setBackgroundColor(0.2, 0.2, 0.2)
 
-local button = Button.new{
-    x = 50,
-    y = 50,
-    width = 200,
-    height = 100
-}
+--local button = Button.new{
+--    x = 50,
+--    y = 50,
+--    width = 200,
+--    height = 100
+--}
 --local image = Image.new{
 --    x = 200,
 --    y = 200,
@@ -45,15 +45,55 @@ local button = Button.new{
 --    x = x + 1
 --end
 
+--local oldX = button:x()
+--button.x = function(self, v)
+--    if v ~= nil then oldX = v end
+--    return oldX + 30 * math.random()
+--end
+
+local mouse = GUI.mouse
+local leftMouseButton = mouse.buttons.left
+
+local x = 0
+local y = 0
+local buttons = {}
+for i = 1, 3000 do
+    buttons[i] = Button.new{
+        x = x,
+        y = y,
+        width = 10,
+        height = 10
+    }
+    local oldX = buttons[i]:x()
+    buttons[i].x = function(self, v)
+        if v ~= nil then oldX = v end
+        return oldX + 3 * math.random()
+    end
+    local oldY = buttons[i]:y()
+    buttons[i].y = function(self, v)
+        if v ~= nil then oldY = v end
+        return oldY + 3 * math.random()
+    end
+    local oldUpdate = buttons[i].update
+    buttons[i].update = function(self)
+        oldUpdate(self)
+        if self:justDraggedBy(leftMouseButton) then
+            self:x(oldX + mouse.xChange)
+            self:y(oldY + mouse.yChange)
+        end
+    end
+    x = x + 10
+    if x > 990 then
+        x = 0
+        y = y + 10
+    end
+end
+
 function GUI.update()
-    button:update()
-    --Image.update(image)
-
-    button:draw()
-    --Image.draw(image, function() Button.draw(button) end)
-
-    button:endUpdate()
-    --Image.endUpdate(image)
+    local numberOfButtons = #buttons
+    for i = 1, numberOfButtons do buttons[i]:update() end
+    for i = 1, numberOfButtons do buttons[i]:draw() end
+    for i = 1, numberOfButtons do buttons[i]:endUpdate() end
 end
 
 GUI.run()
